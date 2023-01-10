1ad238b204a658b30ca3266f54bbd6e480c68685
jchapman63 <96361370+jchapman63@users.noreply.github.com>
2023-01-09 23:23:44 -0500

    Update settings.py

--- flappy_online/settings.py
+++ flappy_online/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3($r+!_cr%g5@-s49f%jxi1b9g#%k2jr&p#w6h6rf=x*r_yg7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
